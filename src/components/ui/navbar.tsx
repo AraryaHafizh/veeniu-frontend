317b7eccfe3d9dedce22958355156584f8d3fd43
AraryaHafizh <araryahafizh@gmail.com>
2025-10-20 13:43:32 +0700

    feat: add route to signin page

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,13 +1,22 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { Button } from "./button";
 
 export const Navbar = () => {
+  const router = useRouter();
+
   return (
     <section className="mx-[52px] my-[20px] flex justify-between">
       <Link className="text-primary text-4xl font-bold" href="/">
         Veeniu
       </Link>
-      <Button variant={"outline_rounded"} size={"sm_rounded"}>
+      <Button
+        variant={"outline_rounded"}
+        size={"sm_rounded"}
+        onClick={() => router.push("/auth/signin")}
+      >
         Sign in
       </Button>
     </section>
